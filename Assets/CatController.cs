fb16d99bf65b02e9bdb692f6bb5050c03ed30918
alicerl <alice.ar.el@gmail.com>
2016-09-11 11:31:06 -0500

    SCG_01
    SpaceCatGarden

---
+++ Assets/CatController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CatController : MonoBehaviour {
+
+		private Rigidbody2D myRigidBody;
+		private Animator myAnim;
+		public float catjumpforce = 500f;
+		// Use this for initialization
+		void Start () {
+			myRigidBody = GetComponent<Rigidbody2D> ();
+			myAnim = GetComponent<Animator> ();
+		}
+
+		// Update is called once per frame
+		void Update () {
+			if (Input.GetButtonUp ("Jump")) 
+			{
+				myRigidBody.AddForce (transform.up * catjumpforce);
+
+			}
+			myAnim.SetFloat ("vVelocity", myRigidBody.velocity.y);
+		}
+
+		void OnCollisionEnter2D(Collision2D collision)
+		{
+			if (collision.collider.gameObject.layer == LayerMask.NameToLayer ("Enemy")) 
+			{
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+
